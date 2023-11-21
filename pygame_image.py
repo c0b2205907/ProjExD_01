9c563392f11d09138874d6be1baddcebde58a533
C0B22059 <c0b2205907@edu.teu.ac.jp>
2023-11-21 17:02:10 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,11 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01/fig/3.png") #練習2 こんかとん画像surfaceの生成
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)]
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,1,1),pg.transform.rotozoom(kk_img,2,1),pg.transform.rotozoom(kk_img,3,1),pg.transform.rotozoom(kk_img,4,1),
+               pg.transform.rotozoom(kk_img,5,1),pg.transform.rotozoom(kk_img,6,1),pg.transform.rotozoom(kk_img,7,1),pg.transform.rotozoom(kk_img,8,1),pg.transform.rotozoom(kk_img,9,1),pg.transform.rotozoom(kk_img,10,1),
+               pg.transform.rotozoom(kk_img,9,1),pg.transform.rotozoom(kk_img,8,1),pg.transform.rotozoom(kk_img,7,1),
+               pg.transform.rotozoom(kk_img,6,1),pg.transform.rotozoom(kk_img,5,1),pg.transform.rotozoom(kk_img,4,1),pg.transform.rotozoom(kk_img,3,1),pg.transform.rotozoom(kk_img,2,1),pg.transform.rotozoom(kk_img,1,1),]
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -18,12 +22,12 @@ def main():
         x = tmr%1600
         screen.blit(bg_img,[-x,0])
         screen.blit(pg.transform.flip(bg_img,True,False),[1600-x,0])
-        screen.blit(bg_img,[1600-x,0])
-        screen.blit(kk_imgs[tmr%2], [300, 200])
+        #screen.blit(bg_img,[1600-x,0])
+        screen.blit(kk_imgs[tmr%len(kk_imgs)], [300, 200])
 
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
